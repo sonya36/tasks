4cbb2b8813e278553ccb7fc6b29ca5adea93a043
Ubuntu <ubuntu@ip-172-31-16-36.ec2.internal>
2026-02-12 10:38:06 +0000

    modify .tf

--- terraform-jenkins/main.tf
+++ terraform-jenkins/main.tf
@@ -1,17 +1,31 @@
-provider "aws"{
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
+
 resource "aws_s3_bucket" "tfstate_bucket" {
   bucket = "soniya-tfstate-s3-bucket"
-  versioning {
-    enabled = true
+}
+
+resource "aws_s3_bucket_versioning" "tfstate_versioning" {
+  bucket = aws_s3_bucket.tfstate_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
-resource "aws_instance" "jenkins" {
-  ami           = "ami-0abcdef1234567890"
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "test_server" {
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
-  tags = {
-    Name = "test-server"
-  }
+  tags = { Name = "test-server" }
 }
